0374894650c0649fdde0c7187ed8cc50bd66f44a
vladzyabs <vladzyaba@gmail.com>
2020-07-16 15:02:28 +0300

    update profileReducer.test.ts

--- src/redux/profilePage/profileReducer.test.ts
+++ src/redux/profilePage/profileReducer.test.ts
@@ -3,19 +3,22 @@ import {v1} from "uuid";
 import * as action from "./profileAction";
 
 let initialState: InitialStateType
+let id1 = v1()
+let id2 = v1()
+let id3 = v1()
 
 beforeEach(() => {
     initialState = {
         posts: [
-            {id: v1(), postValue: 'post 1', countLike: 1},
-            {id: v1(), postValue: 'post 2', countLike: 2},
-            {id: v1(), postValue: 'post 3', countLike: 3},
+            {id: id1, postValue: 'post 1', countLike: 1},
+            {id: id2, postValue: 'post 2', countLike: 2},
+            {id: id3, postValue: 'post 3', countLike: 3},
         ],
         newPosts: ''
     }
 })
 
-test('', () => {
+test('new post should be added', () => {
     let newState
     let newPost = '  post 4  '
 
@@ -27,4 +30,13 @@ test('', () => {
     expect(newState.posts.length).toBe(initialState.posts.length + 1)
     expect(newState.posts[0].postValue).toBe(newPost.trim())
     expect(newState.posts[0].countLike).toBe(0)
+})
+
+test('like post should be added', () => {
+    let newState
+
+    newState = profileReducer(initialState, action.actionAddLikePost(id1))
+
+    expect(newState.posts[0].countLike).toBe(initialState.posts[0].countLike + 1)
+    expect(newState.posts.length).toBe(initialState.posts.length)
 })
