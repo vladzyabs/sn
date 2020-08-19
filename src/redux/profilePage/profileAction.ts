1b9ee740f6f09d4b882980928235b78239e81a91
vladzyabs <vladzyaba@gmail.com>
2020-08-19 21:59:58 +0300

    added thunk for profile page

--- src/redux/profilePage/profileAction.ts
+++ src/redux/profilePage/profileAction.ts
@@ -1,43 +1,50 @@
-import {ProfileInfoType} from "./profileType";
+import {ProfileInfoType} from './profileType'
+import {profileAPI} from '../../api/api';
 
-export const ADD_POST = 'ADD_POST';
-export const INPUT_NEW_POST = 'INPUT_NEW_POST';
-export const ADD_LIKE_POST = 'ADD_LIKE_POST';
-export const SET_USER_INFO = 'SET_USER_INFO';
+export const ADD_POST = 'ADD_POST'
+export const INPUT_NEW_POST = 'INPUT_NEW_POST'
+export const ADD_LIKE_POST = 'ADD_LIKE_POST'
+export const SET_USER_INFO = 'SET_USER_INFO'
 
 type ActionAddPostType = { type: typeof ADD_POST }
 export const actionAddPost = (): ActionAddPostType => {
-    return {
-        type: ADD_POST,
-    }
-};
+   return {
+      type: ADD_POST,
+   }
+}
 
 type ActionInputNewPostType = { type: typeof INPUT_NEW_POST, newPost: string }
 export const actionInputNewPost = (newPost: string): ActionInputNewPostType => {
-    return {
-        type: INPUT_NEW_POST,
-        newPost,
-    }
-};
+   return {
+      type: INPUT_NEW_POST,
+      newPost,
+   }
+}
 
 type ActionAddLikePostType = { type: typeof ADD_LIKE_POST, idPost: string }
 export const actionAddLikePost = (idPost: string): ActionAddLikePostType => {
-    return {
-        type: ADD_LIKE_POST,
-        idPost,
-    }
-};
+   return {
+      type: ADD_LIKE_POST,
+      idPost,
+   }
+}
 
 type ActionSetUserInfoType = { type: typeof SET_USER_INFO, preloader: ProfileInfoType }
 export const actionSetUserInfo = (info: ProfileInfoType): ActionSetUserInfoType => {
-    return {
-        type: SET_USER_INFO,
-        preloader: info,
-    }
-};
+   return {
+      type: SET_USER_INFO,
+      preloader: info,
+   }
+}
+
+export const thunkGetUserInfo = (userID: number) =>
+   (dispatch: any) => {
+      profileAPI.getProfileInfo(userID)
+         .then(response => dispatch(actionSetUserInfo(response.data)))
+   }
 
 export type ProfilePageActionType =
-    ActionAddPostType
-    | ActionInputNewPostType
-    | ActionAddLikePostType
-    | ActionSetUserInfoType
+   ActionAddPostType
+   | ActionInputNewPostType
+   | ActionAddLikePostType
+   | ActionSetUserInfoType
