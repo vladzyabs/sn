d1ece878cc965c8c9c63f862db618ff7f6c94cce
vladz <vladzyaba@gmail.com>
2020-09-14 15:02:27 +0300

    refactoring profile actions

--- src/redux/profilePage/profileAction.ts
+++ src/redux/profilePage/profileAction.ts
@@ -1,64 +1,63 @@
 import { ADD_LIKE_POST, ADD_POST, DELETE_POST, ProfileInfoType, SET_STATUS, SET_USER_INFO } from './profileType'
-import {profileAPI} from '../../api/api'
+import { profileAPI } from '../../api/api'
+import { Dispatch } from 'redux'
 
-type ActionAddPostType = { type: typeof ADD_POST, value: string }
-export const actionAddPost = (value: string): ActionAddPostType => {
-   return {
-      type: ADD_POST,
-      value,
-   }
-}
+// actions =============================================================================================================
 
-type ActionAddLikePostType = { type: typeof ADD_LIKE_POST, idPost: string }
-export const actionAddLikePost = (idPost: string): ActionAddLikePostType => {
-   return {
-      type: ADD_LIKE_POST,
-      idPost,
-   }
-}
-
-type ActionSetUserInfoType = { type: typeof SET_USER_INFO, preloader: ProfileInfoType }
-export const actionSetUserInfo = (info: ProfileInfoType): ActionSetUserInfoType => {
-   return {
-      type: SET_USER_INFO,
-      preloader: info,
-   }
-}
-
-type ActionSetStatusType = { type: typeof SET_STATUS, status: string }
-export const actionSetStatus = (status: string): ActionSetStatusType => {
-   return {
-      type: SET_STATUS,
-      status,
-   }
-}
+export const actionAddPost = (value: string) => ({
+   type: ADD_POST,
+   value,
+} as const)
+type ActionAddPostType = ReturnType<typeof actionAddPost>
 
 export const actionDeletePost = (postID: string) => ({
    type: DELETE_POST,
-   postID
+   postID,
 } as const)
 type DeletePostActionType = ReturnType<typeof actionDeletePost>
 
+export const actionAddLikePost = (idPost: string) => ({
+   type: ADD_LIKE_POST,
+   idPost,
+} as const)
+type ActionAddLikePostType = ReturnType<typeof actionAddLikePost>
+
+
+export const actionSetUserInfo = (info: ProfileInfoType) => ({
+   type: SET_USER_INFO,
+   preloader: info,
+})
+type ActionSetUserInfoType = ReturnType<typeof actionSetUserInfo>
+
+
+export const actionSetStatus = (status: string) => ({
+   type: SET_STATUS,
+   status,
+})
+type ActionSetStatusType = ReturnType<typeof actionSetStatus>
+
+// thunks ==============================================================================================================
+
 export const thunkGetUserInfo = (userID: number) =>
-   (dispatch: any) => {
-      profileAPI.getProfileInfo(userID)
-         .then(response => dispatch(actionSetUserInfo(response.data)))
+   async (dispatch: Dispatch) => {
+      const res = await profileAPI.getProfileInfo(userID)
+      if (res.data) {
+         dispatch(actionSetUserInfo(res.data))
+      }
    }
 
 export const thunkGetStatus = (userID: number) =>
-   (dispatch: any) => {
-      profileAPI.getStatus(userID)
-         .then(response => dispatch(actionSetStatus(response.data)))
+   async (dispatch: Dispatch) => {
+      const res = await profileAPI.getStatus(userID)
+      dispatch(actionSetStatus(res.data))
    }
 
 export const thunkUpdateStatus = (status: string) =>
-   (dispatch: any) => {
-      profileAPI.updateStatus(status)
-         .then(response => {
-            if (response.data.resultCode === 0) {
-               dispatch(actionSetStatus(status))
-            }
-         })
+   async (dispatch: Dispatch) => {
+      const res = await profileAPI.updateStatus(status)
+      if (res.data.resultCode === 0) {
+         dispatch(actionSetStatus(status))
+      }
    }
 
 export type ProfilePageActionType =
