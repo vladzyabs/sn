77525e74e912a7bcd2731f28c032806d5be6279d
vladz <vladzyaba@gmail.com>
2020-09-14 14:55:22 +0300

    refactoring auth

--- src/redux/authReducer/authAction.ts
+++ src/redux/authReducer/authAction.ts
@@ -1,58 +1,47 @@
-import {SET_USER_DATA} from './authTypes'
-import {authAPI} from '../../api/api'
-import {AuthMeDataType} from '../../api/apiType'
-import {Dispatch} from 'redux'
-import {stopSubmit} from 'redux-form'
+import { SET_USER_DATA } from './authTypes'
+import { authAPI } from '../../api/api'
+import { AuthMeDataType } from '../../api/apiType'
+import { Dispatch } from 'redux'
+import { stopSubmit } from 'redux-form'
 
+// actions =============================================================================================================
 
-type SetAuthDataActionType = {
-   type: typeof SET_USER_DATA
-   id: number | null
-   login: string | null
-   email: string | null
-   isAuth: boolean
-}
-export const setAuthDataAC = (payload: AuthMeDataType, isAuth: boolean): SetAuthDataActionType => {
-   return {
-      type: SET_USER_DATA,
-      id: payload.id,
-      login: payload.login,
-      email: payload.email,
-      isAuth: isAuth
-   }
-}
+export const setAuthDataAC = (payload: AuthMeDataType, isAuth: boolean) => ({
+   type: SET_USER_DATA,
+   id: payload.id,
+   login: payload.login,
+   email: payload.email,
+   isAuth: isAuth,
+} as const)
+type SetAuthDataActionType = ReturnType<typeof setAuthDataAC>
+
+// thunks ==============================================================================================================
 
 export const thunkGetAuthData = () =>
-   (dispatch: Dispatch) => {
-      return authAPI.getMe()
-         .then(data => {
-            if (data.resultCode === 0) {
-               dispatch(setAuthDataAC(data.data, true))
-            }
-         })
+   async (dispatch: Dispatch) => {
+      const res = await authAPI.getMe()
+      if (res.resultCode === 0) {
+         dispatch(setAuthDataAC(res.data, true))
+      }
    }
 
 export const thunkLogin = (email: string, password: string, rememberMe: boolean = false) =>
-   (dispatch: any) => {
-      authAPI.login(email, password, rememberMe)
-         .then(response => {
-            if (response.data.resultCode === 0) {
-               dispatch(thunkGetAuthData())
-            } else {
-               let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error'
-               dispatch(stopSubmit('loginForm', {_error: message}))
-            }
-         })
+   async (dispatch: any) => {
+      const res = await authAPI.login(email, password, rememberMe)
+      if (res.data.resultCode === 0) {
+         dispatch(thunkGetAuthData())
+      } else {
+         let message = res.data.messages.length > 0 ? res.data.messages[0] : 'Some error'
+         dispatch(stopSubmit('loginForm', {_error: message}))
+      }
    }
 
 export const thunkLogout = () =>
-   (dispatch: Dispatch) => {
-      authAPI.logout()
-         .then(response => {
-            if (response.data.resultCode === 0) {
-               dispatch(setAuthDataAC({email: null, id: null, login: null}, false))
-            }
-         })
+   async (dispatch: Dispatch) => {
+      const res = await authAPI.logout()
+      if (res.data.resultCode === 0) {
+         dispatch(setAuthDataAC({email: null, id: null, login: null}, false))
+      }
    }
 
 export type AuthActionType
