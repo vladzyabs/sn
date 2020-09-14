e91a8f995342358b21afc62f270be53cd99913ae
vladz <vladzyaba@gmail.com>
2020-09-14 13:59:29 +0300

    tests for dialogs reducer

--- src/redux/dialogsPage/dialogsReducer.test.ts
+++ src/redux/dialogsPage/dialogsReducer.test.ts
@@ -11,7 +11,6 @@ beforeEach(() => {
             {id: v1(), name: 'Cbb'},
             {id: v1(), name: 'Ccc'},
         ],
-        newMessage: '',
         messages: [
             {id: v1(), message: 'Maa', fromMe: false},
             {id: v1(), message: 'Mbb', fromMe: true},
@@ -22,16 +21,10 @@ beforeEach(() => {
 })
 
 test('new message should be added', () => {
-    let newState
-    let newMessage = '  Mee  '
+    const endState = dialogReducer(initialState, action.actionAddMessage('new message'))
 
-    newState = dialogReducer(initialState, action.actionInputNewMessage(newMessage))
-    expect(newState.newMessage).toBe(newMessage)
-
-    newState = dialogReducer(newState, action.actionAddMessage())
-    expect(newState.newMessage).toBe('')
-    expect(newState.messages.length).toBe(initialState.messages.length + 1)
-    expect(newState.chats.length).toBe(initialState.chats.length)
-    expect(newState.messages[newState.messages.length - 1].message).toBe(newMessage.trim())
-    expect(newState.messages[newState.messages.length - 1].fromMe).toBe(true)
+    expect(endState.messages.length).toBe(initialState.messages.length + 1)
+    expect(endState.messages[endState.messages.length - 1].message).toBe('new message')
+    expect(endState.messages[endState.messages.length - 1].fromMe).toBe(true)
+    expect(endState.chats.length).toBe(initialState.chats.length)
 })
