cc368a8f8267595d6dfc4bae0c29889c1d58c981
vladz <vladzyaba@gmail.com>
2020-09-23 16:51:22 +0300

    added paginator

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { UserType } from '../../redux/usersPage/usersType'
 import styles from './Users.module.scss'
 import User from './user'
+import Paginator from '../../components/common/Paginator/Paginator'
 
 type PropsUsersType = {
    users: UserType[]
@@ -16,22 +17,15 @@ type PropsUsersType = {
 }
 
 function Users(props: PropsUsersType) {
-   let pagesCount = Math.ceil(props.totalCount / props.pageSize)
-   let pages = []
-   for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i)
-   }
-
    return (
       <div className={styles.users}>
          <h1>Users</h1>
-         <div>
-            {
-               pages.map(p => {
-                  return <span key={p} className={props.currentPage === p ? styles.selectedPage : ''}
-                               onClick={() => props.onPageChanged(p)}>{p}</span>
-               })
-            }
+         <div className={styles.paginator}>
+            <Paginator onPageChanged={props.onPageChanged}
+                       totalItemsCount={props.totalCount}
+                       currentPage={props.currentPage}
+                       pageSize={props.pageSize}
+                       portionSize={10}/>
          </div>
          {props.users.map(user => <User key={user.id}
                                         user={user}
